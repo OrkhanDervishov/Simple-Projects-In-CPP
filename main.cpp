5333c3f556833b575d6ceb325200f23f004df4b8
OrkhanZero <140115710+Gnome1512@users.noreply.github.com>
2023-11-29 23:43:59 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <Windows.h>
-#include "dog_game.h"
 #include "snake_game.h"
-#include "test.h"
 
 int main(){
 	printf("------------------\n      GAMES\n------------------\n\n");
@@ -13,18 +11,15 @@ int main(){
 	int game;
 	while (1) {
 		printf("Type the number of the game that you want to play: \n");
-		printf("Hungry Dog - 1\nSnake game - 2\nExit - 3\n");
+		printf("Snake game - 1\nExit - 2\n");
 		printf("Enter: ");
 		scanf_s("%d", &game);
 		switch (game)
 		{
 		case 1:
 			system("cls");
-			dog_game();
-		case 2:
-			system("cls");
 			snake();
-		case 3:
+		default:
 			return 0;
 		}
 	}
